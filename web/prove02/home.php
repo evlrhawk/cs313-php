0f4fd3759fb759851b5122506b3f6ed6545c3bb7
Thomas Burr <tb4cmcle@gmail.com>
2018-09-29 13:10:40 -0600

    round 5

---
+++ web/prove02/home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html lang = "en">
+<head>
+   <meta charset = "utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" type="text/css" href="home.css">
+   <title> Thomas Burr's Homepage </title>
+
+</head>
+<body>
+
+<div class="flex1">
+   <!-- Header -->
+   <div id="h" class="HaF">
+      <h1>Welcome to Burrlândia!<h1>
+   </div>
+  
+   <!-- Menu -->
+   <div id="menu" class="flex2">
+      <div><a class="menu" href="assign.html">Assignments</a></div>
+      <div><a class="menu" href="#contact">Contact</a></div>
+   </div>
+
+   <!-- Body -->
+   <div class="flex2">
+      <div class="grow1"></div>
+      <div class="grow2"></div>
+      
+   </div>
+   
+   <!-- Footer -->
+   <div id="contact" class="HaF">
+      <ul>
+         <li>Name:  Thomas Burr</li>
+         <li>Email: bur14027@byui.edu</li>
+         <li>Phone: 540-623-3813</li>
+      </ul>
+   </div>
+</div>
+
+</body>
+</html>
