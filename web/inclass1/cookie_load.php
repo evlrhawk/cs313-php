9507703394ad0a0fd7107e1c2fd15a82cfaa4237
Thomas Burr <tb4cmcle@gmail.com>
2018-10-04 16:04:26 -0600

    cookies

---
+++ web/inclass1/cookie_load.php
@@ -0,0 +1,21 @@
+<?php
+
+   $favorite = $_COOKIE["fav-text"];
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+
+<?php
+
+echo "your favorite text was $favorite";
+
+?>
+
+</body>
+</html>
