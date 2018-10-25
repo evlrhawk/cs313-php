b14b4069bce52cd1be776d53ddf62310891aa165
Thomas Burr <tb4cmcle@gmail.com>
2018-10-25 16:42:24 -0600

    making courses

--- web/inclass2/notes.php
+++ web/inclass2/notes.php
@@ -7,7 +7,7 @@ $course_id = htmlspecialchars($_GET['id']);
 
 $query = 'Select c.code, c.name, n.content, n.id AS note_id FROM note n JOIN course c ON c.id = n.course_id WHERE course_id = :course_id';
 $stmt = $db->prepare($query);
-$stmt->bindValues(":course_id", $course_id, PDO::PARAM_INT);
+$stmt->bindValue(":course_id", $course_id, PDO::PARAM_INT);
 $stmt->execute();
 $courses = $stmt->fetchALL(PDO::FETCH_ASSOC);
 
