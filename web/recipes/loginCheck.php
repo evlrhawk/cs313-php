445738b49908dbf5566d2a799f82b5e3a2221443
Thomas Burr <tb4cmcle@gmail.com>
2018-10-27 09:53:06 -0600

    beginning login feature

---
+++ web/recipes/loginCheck.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+$username = htmlspecialchars($_POST['username']);
+$pwd = htmlspecialchars($_POST['pwd']);
+
+require('dbConnect.php');
+$db = getdb();
+
+$stmt = $db->prepare('SELECT id, username, password FROM users WHERE username = :username;');
+$stmt->bindValue(':username', $username, PDO::PARAM_STR);
+$stmt->execute();
+$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$newpage;
+
+if ($pwd == $user['password']) {
+   $newpage = "recipes.php";
+}
+else{
+   $newpage = "login.php";
+}
+
+header("Location: $newpage");
+die();
+
+?>
