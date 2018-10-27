94a7675ede82e29c8805600bed0556cc67a88799
Thomas Burr <tb4cmcle@gmail.com>
2018-10-27 10:04:17 -0600

    beginning login feature

--- web/recipes/loginCheck.php
+++ web/recipes/loginCheck.php
@@ -8,10 +8,10 @@ $pwd = htmlspecialchars($_POST['pwd']);
 require('dbConnect.php');
 $db = get_db();
 
-$stmt = $db->prepare('SELECT id, username, password FROM users WHERE username = :username;');
+$stmt = $db->prepare("SELECT id, username, password FROM users WHERE username = ':username';");
 $stmt->bindValue(':username', $username, PDO::PARAM_STR);
 $stmt->execute();
-$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 $newpage;
 
