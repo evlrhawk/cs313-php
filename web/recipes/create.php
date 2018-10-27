3d10c8505f0ebdb91b9369c8d1b68cd5b3786ab4
Thomas Burr <tb4cmcle@gmail.com>
2018-10-27 13:45:45 -0600

    beginning login feature

---
+++ web/recipes/create.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+$username = htmlspecialchars($_POST['username']);
+$pwd = htmlspecialchars($_POST['pwd']);
+$newpage;
+
+require('dbConnect.php');
+$db = get_db();
+
+$stmt = $db->prepare("SELECT username FROM users WHERE username = :username;");
+$stmt->bindValue(":username", $username, PDO::PARAM_STR);
+$stmt->execute();
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (isset($user['username'])) {
+   $newpage = "login.php";
+}
+else{
+$stmt = $db->prepare("INSERT INTO users(username, password) VALUES (:user, :pwd)");
+$stmt->bindValue(":user", $username, PDO::PARAM_STR);
+$stmt->bindValue(":pwd", $pwd, PDO::PARAM_STR);
+$stmt->execute();
+
+$newpage = "recipes.php"
+
+$_SESSION["login"] = $db->lastInsertId('users_id_seq');
+}
+
+header("Location: $newpage");
+die();
+
+?>
