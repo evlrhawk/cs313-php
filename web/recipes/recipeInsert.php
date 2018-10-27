3eee7a9e3bca6fd02c5e85aac5679dc4d130754d
Thomas Burr <tb4cmcle@gmail.com>
2018-10-27 15:20:57 -0600

    beginning login feature

--- web/recipes/recipeInsert.php
+++ web/recipes/recipeInsert.php
@@ -6,13 +6,14 @@ $name = htmlspecialchars($_POST['food']);
 $pic = htmlspecialchars($_POST['pic']);
 $category = htmlspecialchars($_POST['cat']);
 $recipe = htmlspecialchars($_POST['recipe']);
+$user_id = htmlspecialchars($_POST['id']);
 
 $newpage;
 
 require('dbConnect.php');
 $db = get_db();
 
-$stmt = $db->prepare("INSERT INTO recipes(name, categories_id, pic, recipe) VALUES (:name, :category, :pic, :recipe)");
+$stmt = $db->prepare("INSERT INTO recipes(user_id, name, categories_id, pic, recipe) VALUES ($user_id, :name, :category, :pic, :recipe)");
 $stmt->bindValue(":name", $name, PDO::PARAM_STR);
 $stmt->bindValue(":category", $category, PDO::PARAM_INT);
 $stmt->bindValue(":pic", $pic, PDO::PARAM_STR);
