93705934d47c69c1e497378e9ae4a661255d0166
Thomas Burr <tb4cmcle@gmail.com>
2018-11-06 15:18:54 -0700

    adding comment addition

--- web/recipes/mail.php
+++ web/recipes/mail.php
@@ -1,9 +1,11 @@
 <?php
 $msg = htmlspecialchars($_POST['message']);
 $sub = htmlspecialchars($_POST['sub']);
+$from = "recipes@recipes.com";
 
-mail("tburr35@yahoo.com", $sub, $msg);
+mail("tburr35@yahoo.com", $sub, $msg, $from);
 
 header("Location: contacts.php");
+die();
 
 ?>
