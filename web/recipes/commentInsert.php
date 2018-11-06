cbfe33d73fe903a8e3dc7c46ff18097cec3aca31
Thomas Burr <tb4cmcle@gmail.com>
2018-11-06 14:06:43 -0700

    adding comment addition

---
+++ web/recipes/commentInsert.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+
+$rating = htmlspecialchars($_POST['rating']);
+$comment = htmlspecialchars($_POST['comment']);
+$user_id = htmlspecialchars($_POST['user_id']);
+$recipe_id = htmlspecialchars($_POST['id'])
+
+$newpage;
+
+require('dbConnect.php');
+$db = get_db();
+
+$stmt = $db->prepare("INSERT INTO comments(user_id, rating, comment, recipe_id) VALUES ($user_id, :rating, :comment, :recipe_id)");
+$stmt->bindValue(":rating", $rating, PDO::PARAM_INT);
+$stmt->bindValue(":recipe_id", $recipe_id, PDO::PARAM_INT);
+$stmt->bindValue(":comment", $comment, PDO::PARAM_STR);
+$stmt->execute();
+
+$newpage = "recipes1.php?id=$recipe_id";
+
+header("Location: $newpage");
+die();
+
+?>
